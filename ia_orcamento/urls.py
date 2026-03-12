02cf9344ce03fa7640fc4a5fa629e341b55d94b0
Sergio <augustgutusergio@gmail.com>
2026-03-11 23:23:34 -0400

    corrige nome da view ia

--- ia_orcamento/urls.py
+++ ia_orcamento/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import gerar_orcamento_ia
+from .views import criar_orcamento_ia
 
 urlpatterns = [
-    path('gerar/', gerar_orcamento_ia),
+    path('gerar/', criar_orcamento_ia),
 ]
